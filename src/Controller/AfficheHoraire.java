beac860a9c1972bcd4c0625535e446df902cbb58
LandryN98 <landrynjohou999@gmail.com>
2021-01-29 15:56:55 +0100

    créer les vues

---
+++ src/Controller/AfficheHoraire.java
@@ -0,0 +1,66 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AfficheHoraire implements Initializable {
+
+    @FXML
+    public void GotoShowSemestre() throws IOException {
+
+    }
+
+    @FXML
+    public void GotoCreateSemestre() throws IOException {
+
+    }
+
+    @FXML
+    public void GotoModifyandDeleteSemestre() throws IOException {
+
+    }
+
+    @FXML
+    public void GotoShowCours() throws IOException {
+
+    }
+
+    @FXML
+    public void GotoCreateCours() throws IOException {
+
+    }
+
+    @FXML
+    public void GotoModifyandDeleteCours() throws IOException {
+
+    }
+
+    @FXML
+    public void GotoShowEtudiant() throws IOException {
+
+    }
+
+    @FXML
+    public void GotoCreateEtudiant() throws IOException {
+
+    }
+
+    @FXML
+    public void GotoModifyandDeleteEtudiant() throws IOException {
+
+    }
+
+    @FXML
+    public void OpenCreateMethod() throws IOException {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
